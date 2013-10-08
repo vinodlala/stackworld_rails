69877a1c5e4db7b53dfb32d6ca8fe346f086e2be
Vinod Lala <vinod_lala@usa.net>
2013-10-08 17:31:34 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StackworldRails::Application.config.secret_key_base = '3f1eb3de9cbd0f5514ae24f9d4168bf5a20bca142dd4356be9a0f541d218471f9cac19b77bb182dec0ef632bb1c6db24422bae0b9c65be4e4e4ab5e4eb1bf0e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StackworldRails::Application.config.session_store :cookie_store, key: '_stackworld_rails_session'
